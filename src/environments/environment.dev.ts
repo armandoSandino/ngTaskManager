fb125aa7db27c1568e086e3af6642a04d2a585df
ArmandoSandino <jsandino@arkkosoft.net>
2023-03-30 18:05:20 -0600

    chore; add environments

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,15 @@
+export const environment = {
+    root: 'dev',
+    production: false,
+    apiURL: 'http://127.0.0.1:8000/api/',
+    apiURLo: 'http://127.0.0.1:8000/api-token-auth/',
+    firebaseConfig: {
+        apiKey: "",
+        authDomain: "",
+        projectId: "",
+        storageBucket: "",
+        messagingSenderId: "",
+        appId: "",
+        measurementId: ""
+    }
+  }
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    root: 'local',
+    production: false,
+    apiURL: 'http://127.0.0.1:8000/api/',
+    apiURLo: 'http://127.0.0.1:8000/api-token-auth/',
+    firebaseConfig: {
+      apiKey: "",
+      authDomain: "",
+      projectId: "",
+      storageBucket: "",
+      messagingSenderId: "",
+      appId: "",
+      measurementId: ""
+    }
+  };
+  
