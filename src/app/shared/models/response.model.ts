aa643576d4523f5737a290fd6a2dce0396ab2c8d
ArmandoSandino <jsandino@arkkosoft.net>
2023-04-10 17:50:20 -0600

    refactor chore; modify models

--- src/app/shared/models/response.model.ts
+++ src/app/shared/models/response.model.ts
@@ -1,7 +1,8 @@
+import { Collaborator } from "./collaborator.model";
 import { Task } from "./task.model";
 
 export class Response {
-    data?: Task[] | Task;
+    data?: Task[] | Task | Collaborator[] | Collaborator;
     constructor( json?: any ){
         this.data = json?.data;
     }
